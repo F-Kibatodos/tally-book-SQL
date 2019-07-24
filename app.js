28c303dfcfa915c8ef7bae0ea4ceef7f13dcd607
Brian.L <brian9500885@gmail.com>
2019-07-24 19:28:02 +0800

    add home and user routers

--- app.js
+++ app.js
@@ -18,30 +18,6 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
-// 認證系統的路由
-// 登入頁面
-app.get('/user/login', (req, res) => {
-  res.render('login')
-})
-// 登入檢查
-app.post('/user/login', (req, res) => {
-  res.send('login')
-})
-// 註冊頁面
-app.get('/user/register', (req, res) => {
-  res.render('register')
-})
-// 註冊檢查
-app.post('/user/register', (req, res) => {
-  User.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  }).then(user => res.redirect('/'))
-})
-// 登出
-app.get('/user/logout', (req, res) => {
-  res.send('logout')
-})
+app.use('/user', require('./routes/user'))
 
 app.listen(3000)
